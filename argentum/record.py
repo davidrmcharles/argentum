fcb1683b0f9b392396106ede4869705a471e12be
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2018-11-26 16:54:17 -0700

    Creating a template for the 'record' view.

--- argentum/record.py
+++ argentum/record.py
@@ -5,34 +5,7 @@ bp = flask.Blueprint('record', __name__)
 @bp.route('/record', methods=['GET', 'POST'])
 def record():
     if flask.request.method == 'GET':
-        return '''\
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>argentum</title>
-  </head>
-  <body>
-    <h1>argentum</h1>
-    <h2>
-      <a href="/status">Status</a> |
-      Record |
-      <a href="/history">History</a>
-    </h2>
-    <form method="post">
-      <label for="date">Date</label><br/>
-      <input type="text" id="date"><br/>
-
-      <label for="amount">Amount</label><br/>
-      <input type="text" id="amount"><br/>
-
-      <label for="description">Description</label><br/>
-      <input type="text" id="description"></br>
-
-      <input type="submit" value="Record"/>
-    </form>
-  </body>
-</html>
-'''
+        return flask.render_template('record/record.html')
 
     if flask.request.method == 'POST':
         return 'Recorded!'
