e1c6bb05e798994bf42741fb9dc1e779450b5b25
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2018-11-26 17:02:17 -0700

    Creating a template for 'status'.

--- argentum/status.py
+++ argentum/status.py
@@ -9,31 +9,14 @@ def index():
 @bp.route('/status', methods=['GET', 'POST'])
 def status():
     if flask.request.method == 'GET':
-        return '''\
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>argentum</title>
-  </head>
-  <body>
-    <h1>argentum</h1>
-    <h2>
-      Status |
-      <a href="/record">Record</a> |
-      <a href="/history">History</a>
-    </h2>
-    <ul>
-      <li>Period: 2018-11-16..2018-11-29</li>
-      <li>Budget: $1200.00</li>
-      <li>Spent: $999.00</li>
-      <li>Remaining: $201.00</li>
-    </ul>
-    <form method="post" onsubmit="return confirm('Are you sure?');">
-      <input type="submit" value="Finalize"/>
-    </form>
-  </body>
-</html>
-'''
+        return flask.render_template(
+            'status/status.html',
+            first_day='2018-11-16',
+            last_day='2018-11-29',
+            budget='$1200.00',
+            spent='$999.00',
+            remaining='$201.00',
+            )
 
     if flask.request.method == 'POST':
         return 'Finalized!'
