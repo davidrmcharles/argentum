e2e66eac240ebe4dd50388c0b1dae3a158cbe72c
davidrmcharles <31576489+davidrmcharles@users.noreply.github.com>
2018-11-26 23:06:40 -0700

    Let's pre-fill the date on the 'record' form.

--- argentum/record.py
+++ argentum/record.py
@@ -1,3 +1,4 @@
+import datetime
 import flask
 
 bp = flask.Blueprint('record', __name__)
@@ -5,7 +6,10 @@ bp = flask.Blueprint('record', __name__)
 @bp.route('/record', methods=['GET', 'POST'])
 def record():
     if flask.request.method == 'GET':
-        return flask.render_template('record/record.html')
+        return flask.render_template(
+            'record/record.html',
+            today=datetime.datetime.now().strftime('%Y-%m-%d')
+            )
 
     if flask.request.method == 'POST':
         return 'Recorded!'
